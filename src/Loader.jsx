addebec15566d360a35cfe09c8d5b64cb778be85
Mayur <mayursonare18@gmail.com>
2024-06-25 16:58:56 +0530

    pagination of jobs

--- src/Loader.jsx
+++ src/Loader.jsx
@@ -3,7 +3,7 @@ import "./Loader.css"
 
 function Loader() {
   return (
-    <div className={`rounded-xl border-2 border-slate-300 bg-white text-white w-full mt-5 mx-auto p-5 border-${"#4D4C5C"} cursor-pointer relative overflow-hidden`}
+    <div className={`rounded-xl border-2 border-slate-300  bg-gray-200 text-gray-200 w-full mt-5 mx-auto p-5 border-${"#4D4C5C"} cursor-pointer relative overflow-hidden`}
     style={{ boxShadow: "0 0 10px rgba(0, 0, 0, 0.1)" }}>
       <div>
         <div className="flex items-center mb-4">
@@ -17,12 +17,12 @@ function Loader() {
           <div className="ml-2 shine-animation  text-lg shine-animation">
           </div>
         </div>
-        <div className="text-3xl font-semibold">"Backend dev"</div>
-        <div className="mt-3">
+        <div className="text-3xl font-semibold bg-gray-300 text-gray-300">"Backend dev"</div>
+        <div className="mt-3 bg-gray-300 text-gray-300">
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vel dignissimos harum alias aperiam voluptate ad 
         </div>
       </div>
-      <div className="flex mt-4 space-x-3 md:space-x-8 h-14">
+      <div className="flex mt-4 space-x-3 md:space-x-8 w-56 rounded-lg h-14 bg-gray-300 text-gray-300">
         
       </div>
     </div>
