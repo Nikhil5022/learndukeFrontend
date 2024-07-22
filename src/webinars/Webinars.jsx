6d411926ac0349c8ad4a3365bbaf6133be951dd2
Mayur <mayursonare18@gmail.com>
2024-07-22 14:15:00 +0530

    ui changes webinar 3

--- src/webinars/Webinars.jsx
+++ src/webinars/Webinars.jsx
@@ -74,6 +74,7 @@ function Webinars() {
         const res = await axios.get(`${import.meta.env.VITE_SERVER_URL}/getUser/${user?.email}`)
         setUserData(res.data)
       }
+      setDisabled(false)
     }
     getUser()
     async function isMentor() {
@@ -84,7 +85,6 @@ function Webinars() {
       setDisabled(false);
     }
     isMentor();
-    setDisabled(false);
     }, [])
 
   const handleGoogleLogin = () => {
@@ -170,7 +170,7 @@ function Webinars() {
     getPast();
   }, [pastPageNumber]);
 
-  return disabled ? <DataFetchLoading /> : (
+  return (disabled && (liveLoading || pastLoading || upLoading)) ? <DataFetchLoading /> : (
     <div className="w-full p-4 flex relative">
       <div className="p-2 my-2  w-full xl:w-4/6">
         { liveWebinars && liveWebinars.length > 0 && 
@@ -507,7 +507,7 @@ const WebinarCard = React.memo(({webinar}) => {
 
   return (
     <div className="md:flex gap-[22px] rounded-2xl border border-gray-300 p-4 md:p-6 my-2 bg-white">
-      <img src={webinar.photo.url} referrerpolicy="no-referrer" alt="" width="363" height="204" className="w-full h-auto md:max-h-[210px] md:w-[339px] rounded-xl object-cover" />
+      <img src={webinar.photo.url} referrerPolicy="no-referrer" alt="" width="363" height="204" className="w-full h-auto md:max-h-[210px] md:w-[339px] rounded-xl object-cover" />
 
       <div className="flex grow flex-col justify-between ml-2 mt-3">
         <Link to={`/detailedWebinar/${webinar._id}`}>
